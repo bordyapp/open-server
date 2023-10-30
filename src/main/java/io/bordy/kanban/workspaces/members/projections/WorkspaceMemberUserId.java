e5371c3fd138932904b332567f43b92897e75347
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-10-31 02:45:07 +0400

    refactor: WorkspaceMemberUserId JavaDoc

--- src/main/java/io/bordy/kanban/workspaces/members/projections/WorkspaceMemberUserId.java
+++ src/main/java/io/bordy/kanban/workspaces/members/projections/WorkspaceMemberUserId.java
@@ -3,6 +3,13 @@ package io.bordy.kanban.workspaces.members.projections;
 import io.bordy.kanban.workspaces.members.WorkspaceMember;
 import io.quarkus.mongodb.panache.common.ProjectionFor;
 
+/**
+ * Projection to extract only {@link WorkspaceMember#getUserId()} from {@link WorkspaceMember} entity.
+ * @param userId to extract
+ *
+ * @author Pavel Bodiachevskii
+ * @since 1.0.0
+ */
 @ProjectionFor(WorkspaceMember.class)
 public record WorkspaceMemberUserId(
         String userId
