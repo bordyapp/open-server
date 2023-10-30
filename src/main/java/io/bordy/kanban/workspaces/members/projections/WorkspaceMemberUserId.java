e3efd48cfd88587781cdc2838a985617f74bb3ca
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-10-31 02:40:47 +0400

    refactor: WorkspaceMemberUserId remove unused import

--- src/main/java/io/bordy/kanban/workspaces/members/projections/WorkspaceMemberUserId.java
+++ src/main/java/io/bordy/kanban/workspaces/members/projections/WorkspaceMemberUserId.java
@@ -3,8 +3,6 @@ package io.bordy.kanban.workspaces.members.projections;
 import io.bordy.kanban.workspaces.members.WorkspaceMember;
 import io.quarkus.mongodb.panache.common.ProjectionFor;
 
-import java.util.UUID;
-
 @ProjectionFor(WorkspaceMember.class)
 public record WorkspaceMemberUserId(
         String userId
