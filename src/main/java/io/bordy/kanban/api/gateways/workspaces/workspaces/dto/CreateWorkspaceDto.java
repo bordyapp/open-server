7ea8a1440f5be26c98d9396808ce9adf1d65435b
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-11-24 02:45:28 +0400

    refactor: WorkspacesGateway create test - forgotten change

--- src/main/java/io/bordy/kanban/api/gateways/workspaces/workspaces/dto/CreateWorkspaceDto.java
+++ src/main/java/io/bordy/kanban/api/gateways/workspaces/workspaces/dto/CreateWorkspaceDto.java
@@ -2,8 +2,10 @@ package io.bordy.kanban.api.gateways.workspaces.workspaces.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.io.Serializable;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record CreateWorkspaceDto(
         String name
-) {
+) implements Serializable /* https://github.com/quarkusio/quarkus/issues/15892 */ {
 }
